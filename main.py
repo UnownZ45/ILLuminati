d01426fead2adb2ed3703ed5466447c667bcb9d1
UnownZ45 <55822394+YungGuyDJ@users.noreply.github.com>
2021-08-12 23:53:27 -0400

    Update main.py
    Optimization update

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-#     Import Machine and Utime Script
-
-from machine import Pin
-import time
-
-#     Import Color Files
-
-import startup
-import clear
-import red
-import purple
+import slavescripts as op
 
 #     Set Pinout Here
 #     Should Be Formatted As Follows
@@ -16,26 +6,40 @@ import purple
 #     or
 #     name = Pin(Pin_Number, Pin_Type)
 
-Switch_1 = Pin(14, Pin.IN, Pin.PULL_DOWN)
-Switch_2 = Pin(14, Pin.IN, Pin.PULL_DOWN)
+Switch_1 = Pin(9, Pin.IN, Pin.PULL_DOWN)
+Switch_2 = Pin(10, Pin.IN, Pin.PULL_DOWN)
+Switch_3 = Pin(14, Pin.IN, Pin.PULL_DOWN)
+Switch_4 = Pin(15, Pin.IN, Pin.PULL_DOWN)
+
+brightness = 1 # 0 is off, 1 is dim, 31 is max
+
+def wheel(offset, brightness):
+    offset = 255 - offset
+    if offset < 85:
+        return (255 - offset * 3, 0, offset *3, brightness)
+    if offset <170:
+        offset -= 85
+        return (0, offset * 3, 255 - offset * 3, 0, brightness)
+    offset -=170
+    return (offset * 3, 255 - offset * 3, brightness)
 
 #     Main Script Goes Here
 
 def main():
-    startup.start()
-    time.sleep(10)
-    clear.clearStrip()
-    
     while True:     #     Runs Script Constantly
         if Switch_1.value():
-            red.change()
+            op.col_red()
+        
         elif Switch_2.value():
-            purple.change()
-        elif Switch_1.value() && Switch_2.value():
-            while True:
-                red.change()
-                time.sleep(2.5)
-                purple.change()
-                time.sleep(2.5)
+            op.rainbow()
+        
+        elif Switch_3.value():
+            op.RGB()
+        
+        elif Switch_4.value():
+            op.rainfade()
+            
         else:
-            clear.clearStrip()
+            op.off()
+
+main()
